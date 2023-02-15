ff4cb3476e649243a734afb0df6d95da31c08f2e
Thomas Wray <twray95@gmail.com>
2023-02-15 11:39:00 -0700

    removed 9 questions cards to try and alter just the 1 over and over in js.  started working on js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,20 +3,26 @@ var testbox = {
   options: ["blue", "red", "green", "purple"],
   answer: "blue",
 };
-
 // console.log(testbox);
 // console.log(typeof testbox);
 // console.log(testbox.options);
 // console.log(typeof testbox.options);
-
 var testArray = ["blue", "red", "green", "purple"];
-
 console.log(testArray);
 console.log(typeof testArray);
 console.log(testArray.at(1));
-
-var optionsArray = [testbox.options]
-
+var optionsArray = [testbox.options.at(0)];
 console.log(optionsArray);
 console.log(typeof optionsArray);
 console.log(optionsArray.at(1));
+
+var option1El = document.querySelector("#option1");
+var option2El = document.querySelector("#option2");
+var option3El = document.querySelector("#option3");
+var option4El = document.querySelector("#option4");
+
+var question1 = {
+  question: "What bracket encloses function code?",
+  options: ["parens", "curly", "square", "none"],
+  answer: "curly",
+};
