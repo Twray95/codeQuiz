e33f0a529ca15ccc6287d3325d5486184f947794
Thomas Wray <twray95@gmail.com>
2023-02-15 14:01:54 -0700

    made click event listener that correctly identifies if the correct answer was clicked vs the wrong answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,36 @@
-var testbox = {
-  question: "what color is the sky?",
-  options: ["blue", "red", "green", "purple"],
-  answer: "blue",
-};
-// console.log(testbox);
-// console.log(typeof testbox);
-// console.log(testbox.options);
-// console.log(typeof testbox.options);
-var testArray = ["blue", "red", "green", "purple"];
-console.log(testArray);
-console.log(typeof testArray);
-console.log(testArray.at(1));
-var optionsArray = [testbox.options.at(0)];
-console.log(optionsArray);
-console.log(typeof optionsArray);
-console.log(optionsArray.at(1));
-
+var questionEl = document.querySelector("#questionActual");
 var option1El = document.querySelector("#option1");
 var option2El = document.querySelector("#option2");
 var option3El = document.querySelector("#option3");
 var option4El = document.querySelector("#option4");
+var answerBoxEl = document.querySelector(".answerBox");
 
 var question1 = {
   question: "What bracket encloses function code?",
   options: ["parens", "curly", "square", "none"],
   answer: "curly",
 };
+
+function fillInQuestionCard() {
+  questionEl.textContent = question1.question;
+  option1El.textContent = question1.options.at(0);
+  option2El.textContent = question1.options.at(1);
+  option3El.textContent = question1.options.at(2);
+  option4El.textContent = question1.options.at(3);
+  answer = question1.options.at(1);
+}
+
+answerBoxEl.addEventListener("click", function (event) {
+  var element = event.target;
+  if (element.classList.contains("answerOption")) {
+    if (element.textContent == answer) {
+      //add reduce timer and move to next question
+      console.log("correct");
+    } else {
+      //move to next answer
+      console.log("wrong");
+    }
+  }
+});
+
+fillInQuestionCard();
