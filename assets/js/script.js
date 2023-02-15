1470db590a2b4d3cfd8d2a13d362a5cfeae625fb
Thomas Wray <twray95@gmail.com>
2023-02-14 22:12:07 -0700

    experimented with js to start creating questions

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+var testbox = {
+  question: "what color is the sky?",
+  options: ["blue", "red", "green", "purple"],
+  answer: "blue",
+};
+
+// console.log(testbox);
+// console.log(typeof testbox);
+// console.log(testbox.options);
+// console.log(typeof testbox.options);
+
+var testArray = ["blue", "red", "green", "purple"];
+
+console.log(testArray);
+console.log(typeof testArray);
+console.log(testArray.at(1));
+
+var optionsArray = [testbox.options.array];
+
+console.log(optionsArray);
+console.log(typeof optionsArray);
+console.log(optionsArray.at(1));
